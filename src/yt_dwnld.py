7f0b542f47b019da80039800df0e7f5f22f549b3
CKeithTaylor <charles.k.taylor75@gmail.com>
2022-05-31 11:45:08 -0500

    Updated url variable and destination folder creation

--- src/yt_dwnld.py
+++ src/yt_dwnld.py
@@ -2,9 +2,14 @@ import os
 from pytube import YouTube
 from pytube import Playlist
 
+### enter the full link to the video or playlist you want to download for the "url" variable ###
+
+url = "##link here##"
+
 
 def pl_dwnld(url):
     p = Playlist(url)
+    set_dir(p.title)
     print(f"Downloading: {p.title}")
     for video in p.video_urls:
         yt = YouTube(video)
@@ -14,20 +19,20 @@ def pl_dwnld(url):
 
 def vid_dwnld(url):
     yt = YouTube(url)
+    set_dir(yt.title)
     print(f"Downloading: {yt.title}")
     yt.streams.get_highest_resolution().download()
 
 
-def set_dir():
+def set_dir(dir):
     try:
-        os.mkdir("C:/youtube_downloads")
-        os.chdir("C:/youtube_downloads")
+        os.mkdir(f"C:/youtube_downloads/{dir}")
+        os.chdir(f"C:/youtube_downloads/{dir}")
     except FileExistsError:
-        os.chdir("C:/youtube_downloads")
+        os.chdir(f"C:/youtube_downloads/{dir}")
 
 
 def main(url):
-    set_dir()
     check = url.split("/")[3]
     if check[0:8] == "playlist":
         pl_dwnld(url)
@@ -36,7 +41,6 @@ def main(url):
 
 
 if __name__ == "__main__":
-    url = "https://www.youtube.com/playlist?list=PLBf0hzazHTGOEuhPQSnq-Ej8jRyXxfYvl"
     main(url)
 
 print("********All Done!********")
